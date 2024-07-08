6712b991bd654c53b19450e904e87e23205796eb
boopathi0708 <boopathirajasadhasivam@gmail.com>
2024-07-08 21:54:57 +0530

    links updated

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -40,13 +40,13 @@ const Footer = () => {
                 <div className="find-us">
                     <div >Find Us</div>
                     <div className="icon-group">
-                        <a href="https://wa.me/916369638806" style={{textDecoration: 'none', color: 'black'}} target="_blank" rel="noopener noreferrer">
+                        <a href="" style={{textDecoration: 'none', color: 'black'}} target="_blank" rel="noopener noreferrer">
                             <IoLogoWhatsapp className='icons whatsapp'/>
                         </a>
-                        <a href="https://www.instagram.com/madhu._.balaji/" style={{textDecoration: 'none', color: 'black'}} target="_blank" rel="noopener noreferrer">
+                        <a href="" style={{textDecoration: 'none', color: 'black'}} target="_blank" rel="noopener noreferrer">
                             <RiInstagramFill className='icons instagram'/>
                         </a>
-                        <a href="https://twitter.com/madhubalaji_vs" style={{textDecoration: 'none', color: 'black'}} target="_blank" rel="noopener noreferrer">
+                        <a href="" style={{textDecoration: 'none', color: 'black'}} target="_blank" rel="noopener noreferrer">
                             <FaSquareXTwitter className='icons twitter'/>
                         </a>
                     </div>
